31a58f839732e31e7a714609d5af9d4eb9421e5c
StanislasE <stanislas.etienne@gmail.com>
2020-11-04 16:18:25 +0100

    modifié

--- ASSETS/js/script2.js
+++ ASSETS/js/script2.js
@@ -4,6 +4,7 @@ document.getElementById('butterfly').onmouseout = setOldImage ;
 function setNewImage()
 {
    document.getElementById('butterfly').src = '/Users/stan/Desktop/Exercices_JS/Partie_2/Exercice_1/ASSETS/Images/Chat-pap.jpg';
+   document.getElementById('butterfly').alt = 'Chat qui chasse un papillon'          
 }
 function setOldImage()
 {
